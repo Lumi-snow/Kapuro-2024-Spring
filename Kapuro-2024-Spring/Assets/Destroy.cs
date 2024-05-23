5230c5e390e199a7e3f1de7ba3675e25afe99a18
lumi <136560713+Lumi-snow@users.noreply.github.com>
2024-05-23 20:35:06 +0900

    Destroyスクリプト
    Destroyスクリプトは書いた。動かん。

---
+++ Kapuro-2024-Spring/Assets/Destroy.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("è’ìÀÅIÅI");
+            Destroy(gameObject);
+        }
+    }
+}
