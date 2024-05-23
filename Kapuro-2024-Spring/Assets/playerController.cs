ba483ad5f32cb4d8a9847c92caed4fe35b7b979e
lumi <136560713+Lumi-snow@users.noreply.github.com>
2024-05-23 17:56:53 +0900

    移動、行動範囲制限
    移動と行動範囲制限

---
+++ Kapuro-2024-Spring/Assets/playerController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+            transform.Translate(-0.1f, 0, 0);
+
+        if (Input.GetKey(KeyCode.RightArrow))
+            transform.Translate(0.1f, 0, 0);
+
+        if (Input.GetKey(KeyCode.UpArrow))
+            transform.Translate(0, 0.1f, 0);
+
+
+        if (Input.GetKey(KeyCode.DownArrow))
+            transform.Translate(0, -0.1f, 0);
+
+
+    }
+}
