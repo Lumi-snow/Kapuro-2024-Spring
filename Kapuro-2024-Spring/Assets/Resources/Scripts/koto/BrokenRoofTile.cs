63ae60143a6e4fbc5e5d33845b387f758850e620
koto-thing <gotoukenta62@gmail.com>
2024-06-11 23:18:06 +0900

    各種RoofTileを変更に合わせて修正 RoofTileDestroyerを追加 RoofTileEvaluaterを追加 ScoreControllerを追加 UIControllerを追加
    とりあえず、ゲームとしての基盤が完成

--- Kapuro-2024-Spring/Assets/Resources/Scripts/koto/BrokenRoofTile.cs
+++ Kapuro-2024-Spring/Assets/Resources/Scripts/koto/BrokenRoofTile.cs
@@ -5,5 +5,13 @@ using UnityEngine;
 //壊れた瓦
 public class BrokenRoofTile : RoofTile
 {
-    
+    public override RoofTileType roofTileType => RoofTileType.BROKEN; // roofTileType プロパティをオーバーライド
+    public override EvaluateType evaluateType { get; set; } // evaluateType プロパティをオーバーライド
+
+    private int scoreBrokenRoofTile = 50; //スコア
+    public override int Score //スコアのプロパティ
+    {
+        get => scoreBrokenRoofTile;
+        set => scoreBrokenRoofTile = value;
+    }
 }
