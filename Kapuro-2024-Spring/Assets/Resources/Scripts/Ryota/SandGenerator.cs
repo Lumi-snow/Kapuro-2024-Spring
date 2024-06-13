f7e34149f13c0694efece62b12d07d9ae412faee
RyotaIzumi <136326820+RyotaIzumi@users.noreply.github.com>
2024-06-13 23:05:45 +0900

    Add level up
    球同士がぶつかると、logに「level up!」と表示され、大きな球が生成されるようにした。
    球のサイズをSandStatus内で決めるようにした。

--- Kapuro-2024-Spring/Assets/Resources/Scripts/Ryota/SandGenerator.cs
+++ Kapuro-2024-Spring/Assets/Resources/Scripts/Ryota/SandGenerator.cs
@@ -6,22 +6,38 @@ public class SandGenerator : MonoBehaviour
 {
     public GameObject sandPrefab;
     public GameObject Player;
+    static bool canCreateSand = false;
 
-    void start()
+    void Start()
     {
         this.Player = GameObject.Find("Player");
     }
 
     void Update()
     {
-        int rnd = Random.Range(0, 3);
-        int size = 20 + rnd * 15;
+        int rnd = Random.Range(1, 4);
         if (Input.GetKeyDown(KeyCode.Space))
         {
             GameObject sand = Instantiate(sandPrefab);
             sand.transform.position = new Vector3(Player.transform.position.x, Player.transform.position.y - 20, 500);
-            sand.transform.localScale = new Vector3(size, size, size);
             sand.GetComponent<SandStatus>().sizeLevel = rnd;
+        }
+    }
+
+    public void createLevelUpSand(GameObject sandA, GameObject sandB)
+    {
+        if (!canCreateSand) canCreateSand = true;
+        else
+        {
+            Debug.Log("LevelUP!!!");
+            float disX = (sandB.transform.position.x - sandA.transform.position.x) / 2;
+            float disY = (sandB.transform.position.y - sandA.transform.position.y) / 2;
+
+            GameObject sand = Instantiate(sandPrefab);
+            sand.transform.position = new Vector3(sandA.transform.position.x + disX, sandA.transform.position.y + disY, 500);
+            sand.GetComponent<SandStatus>().sizeLevel = sandA.GetComponent<SandStatus>().sizeLevel + 1;
+
+            canCreateSand = false;
         }
     }
 }
