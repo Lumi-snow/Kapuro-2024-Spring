cbc1e81dfbade2eba8cde1fdb334e6e2cbe1e4ab
botamochi1111 <s1310194@u-aizu.ac.jp>
2024-06-21 10:03:08 +0900

    Add sikkuiStatus

--- Kapuro-2024-Spring/Assets/Resources/Scripts/Botamochi/ShikkuiManager.cs
+++ Kapuro-2024-Spring/Assets/Resources/Scripts/Botamochi/ShikkuiManager.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ShikkuiGenerator : MonoBehaviour
+public class ShikkuiManager : MonoBehaviour
 {
+    private ShikkuiStatus shikkuiStatus;
     public GameObject ShikkuiPrefab;
     GameObject Kura;
     GameObject Player;
@@ -16,6 +17,8 @@ public class ShikkuiGenerator : MonoBehaviour
 
     void Start()
     {
+        this.shikkuiStatus = new ShikkuiStatus();
+
         VariableCollection variableCollection = GameObject.Find("VariableCollection").GetComponent<VariableCollection>();
         this.scale_kura = variableCollection.scale_kura;
         this.scale_shikkui = variableCollection.scale_shikkui;
@@ -30,17 +33,44 @@ public class ShikkuiGenerator : MonoBehaviour
 
     void Update()
     {
+        int i = shikkuiStatus.Status;
         //nullチェック
         if (Player == null)
         {
-            gameObject.GetComponent<ShikkuiGenerator>().enabled = false;
+            gameObject.GetComponent<ShikkuiManager>().enabled = false;
         }
 
         //Aを押したら漆喰を壁に貼り付ける
-        if (Input.GetKeyDown(KeyCode.A))
+        if (i == 0)
         {
-            GenerateShikkui(0);
-            Debug.Log("a");
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                GenerateShikkui(0);
+                shikkuiStatus.Status = 1;
+                Debug.Log(shikkuiStatus.Status);
+            }
+        }
+
+        //Aを押したら漆喰を塗る
+        else if (i == 1)
+        {
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                //GenerateShikkui(1);
+                shikkuiStatus.Status = 0;
+                Debug.Log(shikkuiStatus.Status);
+            }
+        }
+
+        //Aを押したら漆喰をこて板に乗せる
+        else if (i == 2)
+        {
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                GenerateShikkui(1);
+                shikkuiStatus.Status = 0;
+                Debug.Log(shikkuiStatus.Status);
+            }
         }
     }
 
@@ -57,6 +87,9 @@ public class ShikkuiGenerator : MonoBehaviour
 
             go.transform.position = new Vector3(x, y, z);
 
+
         }
+
+        
     }
 }
